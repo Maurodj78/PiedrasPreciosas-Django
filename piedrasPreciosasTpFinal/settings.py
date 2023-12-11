e41758923f471bdb3ffdfac596cb620c46a5f8d1
Mauro Alonzi <mauroalonzi@gmail.com>
2023-12-11 00:01:26 -0300

    Configuraciones en settings para pythonanywhere

--- piedrasPreciosasTpFinal/settings.py
+++ piedrasPreciosasTpFinal/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-*!o=f*b0n*+dx9^@!1_!klq7+e^#29s^%4flv$7e_*&a-xicg%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["maurodj78.pythonanywhere.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
